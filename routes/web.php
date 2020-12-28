bf4cfb56766bfd6c9485bd8abb9ff2715631f569
sofion <sofion_38@hotmail.com>
2020-12-28 22:53:43 +0700

    new commit

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/* Route::get('/', 'PageController@home');
+Route::get('/', 'PageController@home');
 Route::get('home', 'PageController@home');
 Route::get('about', 'PageController@about');
-Route::get('contact', 'PageController@contact'); */
+Route::get('contact', 'PageController@contact');
